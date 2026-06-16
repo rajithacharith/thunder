thunder-id/thunderid

Add IDP attribute configuration for token exchange and federated login
External IDP claims were passed through verbatim, so issued tokens carried
the right claims only if the external names happened to match what ThunderID
expects.

Add a dedicated ATTRIBUTE_CONFIGURATION column on the IDP (a structured config
common to every IDP type, separate from the type-specific properties). It has
two parts: how an incoming identity resolves to a local user type, and
per-user-type attribute mappings from external attributes to local attributes:

  "attributeConfiguration": {
    "userTypeResolution": { "default": "person" },
    "userTypeAttributeMappings": [
      {
        "userType": "person",
        "attributes": [
          { "externalAttribute": "given_name", "localAttribute": "firstName" },
          { "externalAttribute": "address.email", "localAttribute": "email" }
        ]
      }
    ]
  }

- userTypeResolution resolves the local user type. This iteration supports
  default only (a fixed type); claim-driven resolution is additive later.
- externalAttribute (source) may be a dot-notation path into a nested claim; a
  claim name containing dots is matched literally first (URI-style claims).
- localAttribute (target) must be defined in the profile's userType schema,
  validated at IDP create/update time via the entity type service.
- A single external attribute may map to multiple local attributes; mapping two
  external attributes to the same local attribute is a conflict and is rejected.
  A user type may have at most one profile.
- Targets are top-level attributes in this iteration; nested target writes are
  deferred.

The attributeConfiguration envelope (userTypeResolution + userTypeAttributeMappings)
is deliberately nested so later additions are non-breaking: claim-driven
user-type resolution, per-attribute transforms and sync/account-linking
policy, and multiple profiles per IDP.

Applied in two places, sharing idp.GetAttributeMappings / idp.ApplyAttributeMapp

backend/internal/idp/service.go
@@ -22,10 +22,13 @@ package idp
 import (
 	"context"
 	"errors"
+	"fmt"
 	"strings"
 
+	"github.com/thunder-id/thunderid/internal/entitytype"
 	declarativeresource "github.com/thunder-id/thunderid/internal/system/declarative_resource"
 	"github.com/thunder-id/thunderid/internal/system/error/serviceerror"
+	"github.com/thunder-id/thunderid/internal/system/i18n/core"
 	"github.com/thunder-id/thunderid/internal/system/log"
 	"github.com/thunder-id/thunderid/internal/system/transaction"
 	"github.com/thunder-id/thunderid/internal/system/utils"
@@ -45,20 +48,89 @@ type IDPServiceInterface interface {
 
 // idpService is the default implementation of the IdPServiceInterface.
 type idpService struct {
-	idpStore      idpStoreInterface
-	transactioner transaction.Transactioner
-	logger        *log.Logger
-	uuidGenerator func() (string, error)
+	idpStore          idpStoreInterface
+	entityTypeService entitytype.EntityTypeServiceInterface
+	transactioner     transaction.Transactioner
+	logger            *log.Logger
+	uuidGenerator     func() (string, error)
 }
 
 // newIDPService creates a new instance of IdPService.
-func newIDPService(idpStore idpStoreInterface, transactioner transaction.Transactioner) IDPServiceInterface {
+func newIDPService(idpStore idpStoreInterface, entityTypeService entitytype.EntityTypeServiceInterface,
+	transactioner transaction.Transactioner) IDPServiceInterface {
 	return &idpService{
-		idpStore:      idpStore,
-		transactioner: transactioner,
-		logger:        log.GetLogger().With(log.String(log.LoggerKeyComponentName, "IdPService")),
-		uuidGenerator: utils.GenerateUUIDv7,
+		idpStore:          idpStore,
+		entityTypeService: entityTypeService,
+		transactioner:     transactioner,
+		logger:            log.GetLogger().With(log.String(log.LoggerKeyComponentName, "IdPService")),
+		uuidGenerator:     utils.GenerateUUIDv7,
 	}
+}
+
+// validateAttributeConfiguration validates the IDP's attribute configuration: a required default user type, and
+// for each user type's attributes a valid claim-mapping shape with every local (target) claim a
+// non-credential attribute defined in that user type's schema. No-op when no profile is configured.
+func (is *idpService) validateAttributeConfiguration(ctx context.Context, idp *IDPDTO) *serviceerror.ServiceError {
+	profile := idp.AttributeConfiguration
+	if profile == nil {
+		return nil
+	}
+	if profile.UserTypeResolution == nil || strings.TrimSpace(profile.UserTypeResolution.Default) == "" {
+		return serviceerror.CustomServiceError(ErrorInvalidAttributeConfiguration, core.I18nMessage{
+			Key:          "error.idpservice.attribute_configuration_user_type_required_description",
+			DefaultValue: "attribute configuration requires a user type",
+		})
+	}
+
+	seenUserTypes := make(map[string]bool, len(profile.UserTypeAttributeMappings))
+	for i := range profile.UserTypeAttributeMappings {
+		entry := profile.UserTypeAttributeMappings[i]
+		if strings.TrimSpace(entry.UserType) == "" {
+			return serviceerror.CustomServiceError(ErrorInvalidAttributeConfiguration, core.I18nMessage{
+				Key:          "error.idpservice.attribute_configuration_entry_user_type_required_description",
+				DefaultValue: "each user type attributes entry requires a user type",
+			})
+		}
+		if seenUserTypes[entry.UserType] {
+			return serviceerror.CustomServiceError(ErrorInvalidAttributeConfiguration, core.I18nMessage{
+				Key: "error.idpservice.attribute_configuration_duplicate_user_type_description",
+				DefaultValue: fmt.Sprintf(
+					"user type '%s' is configured more than once", entry.UserType),
+			})
+		}
+		seenUserTypes[entry.UserType] = true
+
+		if len(entry.Attributes) > 0 {
+			if svcErr := validateAttributeMappingShape(entry.Attributes); svcErr != nil {
+				return svcErr
+			}
+		}
+
+		// Local targets must be non-credential attributes defined in the user type's schema.
+		attributes, svcErr := is.entityTypeService.GetAttributes(
+			ctx, entitytype.TypeCategoryUser, entry.UserType, false, true, false)
+		if svcErr != nil {
+			return serviceerror.CustomServiceError(ErrorInvalidAttributeConfiguration, core.I18nMessage{
+				Key: "error.idpservice.attribute_configuration_user_type_invalid_description",
+				DefaultValue: fmt.Sprintf("invalid user type '%s' for attribute configuration: %s",
+					entry.UserType, svcErr.ErrorDescription.DefaultValue),
+			})
+		}
+		validTargets := make(map[string]bool, len(attributes))
+		for _, attr := range attributes {
+			validTargets[attr.Attribute] = true
+		}
+		for _, m := range entry.Attributes {
+			if !validTargets[m.LocalAttribute] {
+				return serviceerror.CustomServiceError(ErrorInvalidAttributeConfiguration, core.I18nMessage{
+					Key: "error.idpservice.attribute_configuration_target_not_in_schema_description",
+					DefaultValue: fmt.Sprintf("local claim '%s' is not an attribute of user type '%s'",
+						m.LocalAttribute, entry.UserType),
+				})
+			}
+		}
+	}
+	return nil
 }
 
 // CreateIdentityProvider creates a new Identity Provider.
@@ -70,6 +142,9 @@ func (is *idpService) CreateIdentityProvider(
 	}
 
 	if svcErr := validateIDP(ctx, idp, logger); svcErr != nil {
+		return nil, svcErr
+	}
+	if svcErr := is.validateAttributeConfiguration(ctx, idp); svcErr != nil {
 		return nil, svcErr
 	}
 
@@ -208,6 +283,9 @@ func (is *idpService) UpdateIdentityProvider(ctx context.Context, idpID string, 
 		return nil, &ErrorInvalidIDPID
 	}
 	if svcErr := validateIDP(ctx, idp, logger); svcErr != nil {
+		return nil, svcErr
+	}
+	if svcErr := is.validateAttributeConfiguration(ctx, idp); svcErr != nil {
 		return nil, svcErr
 	}
 

backend/internal/idp/utils_test.go
@@ -727,3 +727,126 @@ func (s *IDPUtilsTestSuite) TestValidateIDPProperties_SecretPropertyValueUnreada
 	s.NotNil(err)
 	s.Equal(ErrorInvalidIDPProperty.Code, err.Code)
 }
+
+func (s *IDPUtilsTestSuite) TestApplyAttributeMappings_NilMappings_NoOp() {
+	attrs := map[string]interface{}{"email": "user@example.com"}
+	result := ApplyAttributeMappings(attrs, nil)
+	s.Equal(attrs, result)
+}
+
+func (s *IDPUtilsTestSuite) TestApplyAttributeMappings_EmptyMappings_NoOp() {
+	attrs := map[string]interface{}{"email": "user@example.com"}
+	result := ApplyAttributeMappings(attrs, []AttributeMapping{})
+	s.Equal(attrs, result)
+}
+
+func (s *IDPUtilsTestSuite) TestApplyAttributeMappings_RenamesMappedClaim() {
+	attrs := map[string]interface{}{
+		"http://schemas.example.com/emailaddress": "user@example.com",
+	}
+	result := ApplyAttributeMappings(attrs, []AttributeMapping{
+		{ExternalAttribute: "http://schemas.example.com/emailaddress", LocalAttribute: "email"},
+	})
+	s.Equal("user@example.com", result["email"])
+	_, present := result["http://schemas.example.com/emailaddress"]
+	s.False(present)
+}
+
+func (s *IDPUtilsTestSuite) TestApplyAttributeMappings_OneSourceToMultipleTargets() {
+	attrs := map[string]interface{}{"email": "user@example.com"}
+	result := ApplyAttributeMappings(attrs, []AttributeMapping{
+		{ExternalAttribute: "email", LocalAttribute: "email"},
+		{ExternalAttribute: "email", LocalAttribute: "contactEmail"},
+	})
+	s.Equal("user@example.com", result["email"])
+	s.Equal("user@example.com", result["contactEmail"])
+}
+
+func (s *IDPUtilsTestSuite) TestApplyAttributeMappings_UnmappedPassesThrough() {
+	attrs := map[string]interface{}{
+		"given_name": "Jane",
+		"department": "engineering",
+	}
+	result := ApplyAttributeMappings(
+		attrs, []AttributeMapping{{ExternalAttribute: "given_name", LocalAttribute: "firstName"}})
+	s.Equal("Jane", result["firstName"])
+	s.Equal("engineering", result["department"])
+	_, present := result["given_name"]
+	s.False(present)
+}
+
+func (s *IDPUtilsTestSuite) TestApplyAttributeMappings_MappedValueOverridesCollision() {
+	attrs := map[string]interface{}{
+		"given_name": "Jane",
+		"firstName":  "stale",
+	}
+	result := ApplyAttributeMappings(
+		attrs, []AttributeMapping{{ExternalAttribute: "given_name", LocalAttribute: "firstName"}})
+	s.Equal("Jane", result["firstName"])
+}
+
+func (s *IDPUtilsTestSuite) TestApplyAttributeMappings_MissingExternalClaimIgnored() {
+	attrs := map[string]interface{}{"email": "user@example.com"}
+	result := ApplyAttributeMappings(
+		attrs, []AttributeMapping{{ExternalAttribute: "given_name", LocalAttribute: "firstName"}})
+	s.Equal("user@example.com", result["email"])
+	_, present := result["firstName"]
+	s.False(present)
+}
+
+func (s *IDPUtilsTestSuite) TestApplyAttributeMappings_NestedSourcePath() {
+	attrs := map[string]interface{}{
+		"address": map[string]interface{}{
+			"email": "user@example.com",
+		},
+		"keep": "value",
+	}
+	result := ApplyAttributeMappings(
+		attrs, []AttributeMapping{{ExternalAttribute: "address.email", LocalAttribute: "email"}})
+	s.Equal("user@example.com", result["email"])
+	// The containing nested object is read non-destructively and passes through.
+	s.Equal(attrs["address"], result["address"])
+	s.Equal("value", result["keep"])
+}
+
+func (s *IDPUtilsTestSuite) TestApplyAttributeMappings_DottedKeyMatchedLiterallyBeforePath() {
+	// A URI-style claim name containing dots must be matched literally, not split into a path.
+	attrs := map[string]interface{}{
+		"http://schemas.example.com/emailaddress": "user@example.com",
+	}
+	result := ApplyAttributeMappings(attrs, []AttributeMapping{
+		{ExternalAttribute: "http://schemas.example.com/emailaddress", LocalAttribute: "email"},
+	})
+	s.Equal("user@example.com", result["email"])
+}
+
+func (s *IDPUtilsTestSuite) TestGetAttributeMappings_NilIDP() {
+	s.Nil(GetAttributeMappings(nil))
+}
+
+func (s *IDPUtilsTestSuite) TestGetAttributeMappings_NilAttributeConfiguration() {
+	s.Nil(GetAttributeMappings(&IDPDTO{}))
+}
+
+func (s *IDPUtilsTestSuite) TestGetAttributeMappings_ReturnsMappings() {
+	mappings := []AttributeMapping{{ExternalAttribute: "given_name", LocalAttribute: "firstName"}}
+	idpDTO := &IDPDTO{AttributeConfiguration: &AttributeConfiguration{
+		UserTypeResolution:        &UserTypeResolution{Default: "person"},
+		UserTypeAttributeMappings: []UserTypeAttributeMapping{{UserType: "person", Attributes: mappings}},
+	}}
+	s.Equal(mappings, GetAttributeMappings(idpDTO))
+}
+
+func (s *IDPUtilsTestSuite) TestGetMappedUserType_NilIDP() {
+	s.Equal("", GetMappedUserType(nil))
+}
+
+func (s *IDPUtilsTestSuite) TestGetMappedUserType_NilAttributeConfiguration() {
+	s.Equal("", GetMappedUserType(&IDPDTO{}))
+}
+
+func (s *IDPUtilsTestSuite) TestGetMappedUserType_ReturnsUserType() {
+	s.Equal("person", GetMappedUserType(&IDPDTO{AttributeConfiguration: &AttributeConfiguration{
+		UserTypeResolution: &UserTypeResolution{Default: "person"},
+	}}))
+}

rajithacharith/thunder

backend/internal/idp/service.go
@@ -22,10 +22,13 @@ package idp
 import (
 	"context"
 	"errors"
+	"fmt"
 	"strings"
 
+	"github.com/thunder-id/thunderid/internal/entitytype"
 	declarativeresource "github.com/thunder-id/thunderid/internal/system/declarative_resource"
 	"github.com/thunder-id/thunderid/internal/system/error/serviceerror"
+	"github.com/thunder-id/thunderid/internal/system/i18n/core"
 	"github.com/thunder-id/thunderid/internal/system/log"
 	"github.com/thunder-id/thunderid/internal/system/transaction"
 	"github.com/thunder-id/thunderid/internal/system/utils"
@@ -45,19 +48,22 @@ type IDPServiceInterface interface {
 
 // idpService is the default implementation of the IdPServiceInterface.
 type idpService struct {
-	idpStore      idpStoreInterface
-	transactioner transaction.Transactioner
-	logger        *log.Logger
-	uuidGenerator func() (string, error)
+	idpStore          idpStoreInterface
+	entityTypeService entitytype.EntityTypeServiceInterface
+	transactioner     transaction.Transactioner
+	logger            *log.Logger
+	uuidGenerator     func() (string, error)
 }
 
 // newIDPService creates a new instance of IdPService.
-func newIDPService(idpStore idpStoreInterface, transactioner transaction.Transactioner) IDPServiceInterface {
+func newIDPService(idpStore idpStoreInterface, entityTypeService entitytype.EntityTypeServiceInterface,
+	transactioner transaction.Transactioner) IDPServiceInterface {
 	return &idpService{
-		idpStore:      idpStore,
-		transactioner: transactioner,
-		logger:        log.GetLogger().With(log.String(log.LoggerKeyComponentName, "IdPService")),
-		uuidGenerator: utils.GenerateUUIDv7,
+		idpStore:          idpStore,
+		entityTypeService: entityTypeService,
+		transactioner:     transactioner,
+		logger:            log.GetLogger().With(log.String(log.LoggerKeyComponentName, "IdPService")),
+		uuidGenerator:     utils.GenerateUUIDv7,
 	}
 }
 
@@ -70,6 +76,9 @@ func (is *idpService) CreateIdentityProvider(
 	}
 
 	if svcErr := validateIDP(ctx, idp, logger); svcErr != nil {
+		return nil, svcErr
+	}
+	if svcErr := is.validateAttributeConfiguration(ctx, idp); svcErr != nil {
 		return nil, svcErr
 	}
 
@@ -210,6 +219,9 @@ func (is *idpService) UpdateIdentityProvider(ctx context.Context, idpID string, 
 	if svcErr := validateIDP(ctx, idp, logger); svcErr != nil {
 		return nil, svcErr
 	}
+	if svcErr := is.validateAttributeConfiguration(ctx, idp); svcErr != nil {
+		return nil, svcErr
+	}
 
 	idp.ID = idpID
 	var svcErr *serviceerror.ServiceError
@@ -303,5 +315,71 @@ func (is *idpService) DeleteIdentityProvider(ctx context.Context, idpID string) 
 		return &serviceerror.InternalServerError
 	}
 
+	return nil
+}
+
+// validateAttributeConfiguration validates the IDP's attribute configuration: a required default user type, and
+// for each user type's attributes a valid claim-mapping shape with every local (target) claim a
+// non-credential attribute defined in that user type's schema. No-op when no profile is configured.
+func (is *idpService) validateAttributeConfiguration(ctx context.Context, idp *IDPDTO) *serviceerror.ServiceError {
+	profile := idp.AttributeConfiguration
+	if profile == nil {
+		return nil
+	}
+	if profile.UserTypeResolution == nil || strings.TrimSpace(profile.UserTypeResolution.Default) == "" {
+		return serviceerror.CustomServiceError(ErrorInvalidAttributeConfiguration, core.I18nMessage{
+			Key:          "error.idpservice.attribute_configuration_user_type_required_description",
+			DefaultValue: "attribute configuration requires an user type",
+		})
+	}
+
+	seenUserTypes := make(map[string]bool, len(profile.UserTypeAttributeMappings))
+	for i := range profile.UserTypeAttributeMappings {
+		entry := profile.UserTypeAttributeMappings[i]
+		if strings.TrimSpace(entry.UserType) == "" {
+			return serviceerror.CustomServiceError(ErrorInvalidAttributeConfiguration, core.I18nMessage{
+				Key:          "error.idpservice.attribute_configuration_entry_user_type_required_description",
+				DefaultValue: "each user type attributes entry requires an user type",
+			})
+		}
+		if seenUserTypes[entry.UserType] {
+			return serviceerror.CustomServiceError(ErrorInvalidAttributeConfiguration, core.I18nMessage{
+				Key: "error.idpservice.attribute_configuration_duplicate_user_type_description",
+				DefaultValue: fmt.Sprintf(
+					"user type '%s' is configured more than once", entry.UserType),
+			})
+		}
+		seenUserTypes[entry.UserType] = true
+
+		if len(entry.Attributes) > 0 {
+			if svcErr := validateAttributeMappingShape(entry.Attributes); svcErr != nil {
+				return svcErr
+			}
+		}
+
+		// Local targets must be non-credential attributes defined in the user type's schema.
+		attributes, svcErr := is.entityTypeService.GetAttributes(
+			ctx, entitytype.TypeCategoryUser, entry.UserType, false, true, false)
+		if svcErr != nil {
+			return serviceerror.CustomServiceError(ErrorInvalidAttributeConfiguration, core.I18nMessage{
+				Key: "error.idpservice.attribute_configuration_user_type_invalid_description",
+				DefaultValue: fmt.Sprintf("invalid user type '%s' for attribute configuration: %s",
+					entry.UserType, svcErr.ErrorDescription.DefaultValue),
+			})
+		}
+		validTargets := make(map[string]bool, len(attributes))
+		for _, attr := range attributes {
+			validTargets[attr.Attribute] = true
+		}
+		for _, m := range entry.Attributes {
+			if !validTargets[m.LocalAttribute] {
+				return serviceerror.CustomServiceError(ErrorInvalidAttributeConfiguration, core.I18nMessage{
+					Key: "error.idpservice.attribute_configuration_target_not_in_schema_description",
+					DefaultValue: fmt.Sprintf("local claim '%s' is not an attribute of user type '%s'",
+						m.LocalAttribute, entry.UserType),
+				})
+			}
+		}
+	}
 	return nil
 }

backend/internal/idp/utils_test.go
@@ -727,3 +727,126 @@ func (s *IDPUtilsTestSuite) TestValidateIDPProperties_SecretPropertyValueUnreada
 	s.NotNil(err)
 	s.Equal(ErrorInvalidIDPProperty.Code, err.Code)
 }
+
+func (s *IDPUtilsTestSuite) TestApplyAttributeMappings_NilMappings_NoOp() {
+	attrs := map[string]interface{}{"email": "user@example.com"}
+	result := ApplyAttributeMappings(attrs, nil)
+	s.Equal(attrs, result)
+}
+
+func (s *IDPUtilsTestSuite) TestApplyAttributeMappings_EmptyMappings_NoOp() {
+	attrs := map[string]interface{}{"email": "user@example.com"}
+	result := ApplyAttributeMappings(attrs, []AttributeMapping{})
+	s.Equal(attrs, result)
+}
+
+func (s *IDPUtilsTestSuite) TestApplyAttributeMappings_RenamesMappedClaim() {
+	attrs := map[string]interface{}{
+		"http://schemas.example.com/emailaddress": "user@example.com",
+	}
+	result := ApplyAttributeMappings(attrs, []AttributeMapping{
+		{ExternalAttribute: "http://schemas.example.com/emailaddress", LocalAttribute: "email"},
+	})
+	s.Equal("user@example.com", result["email"])
+	_, present := result["http://schemas.example.com/emailaddress"]
+	s.False(present)
+}
+
+func (s *IDPUtilsTestSuite) TestApplyAttributeMappings_OneSourceToMultipleTargets() {
+	attrs := map[string]interface{}{"email": "user@example.com"}
+	result := ApplyAttributeMappings(attrs, []AttributeMapping{
+		{ExternalAttribute: "email", LocalAttribute: "email"},
+		{ExternalAttribute: "email", LocalAttribute: "contactEmail"},
+	})
+	s.Equal("user@example.com", result["email"])
+	s.Equal("user@example.com", result["contactEmail"])
+}
+
+func (s *IDPUtilsTestSuite) TestApplyAttributeMappings_UnmappedPassesThrough() {
+	attrs := map[string]interface{}{
+		"given_name": "Jane",
+		"department": "engineering",
+	}
+	result := ApplyAttributeMappings(
+		attrs, []AttributeMapping{{ExternalAttribute: "given_name", LocalAttribute: "firstName"}})
+	s.Equal("Jane", result["firstName"])
+	s.Equal("engineering", result["department"])
+	_, present := result["given_name"]
+	s.False(present)
+}
+
+func (s *IDPUtilsTestSuite) TestApplyAttributeMappings_MappedValueOverridesCollision() {
+	attrs := map[string]interface{}{
+		"given_name": "Jane",
+		"firstName":  "stale",
+	}
+	result := ApplyAttributeMappings(
+		attrs, []AttributeMapping{{ExternalAttribute: "given_name", LocalAttribute: "firstName"}})
+	s.Equal("Jane", result["firstName"])
+}
+
+func (s *IDPUtilsTestSuite) TestApplyAttributeMappings_MissingExternalClaimIgnored() {
+	attrs := map[string]interface{}{"email": "user@example.com"}
+	result := ApplyAttributeMappings(
+		attrs, []AttributeMapping{{ExternalAttribute: "given_name", LocalAttribute: "firstName"}})
+	s.Equal("user@example.com", result["email"])
+	_, present := result["firstName"]
+	s.False(present)
+}
+
+func (s *IDPUtilsTestSuite) TestApplyAttributeMappings_NestedSourcePath() {
+	attrs := map[string]interface{}{
+		"address": map[string]interface{}{
+			"email": "user@example.com",
+		},
+		"keep": "value",
+	}
+	result := ApplyAttributeMappings(
+		attrs, []AttributeMapping{{ExternalAttribute: "address.email", LocalAttribute: "email"}})
+	s.Equal("user@example.com", result["email"])
+	// The containing nested object is read non-destructively and passes through.
+	s.Equal(attrs["address"], result["address"])
+	s.Equal("value", result["keep"])
+}
+
+func (s *IDPUtilsTestSuite) TestApplyAttributeMappings_DottedKeyMatchedLiterallyBeforePath() {
+	// A URI-style claim name containing dots must be matched literally, not split into a path.
+	attrs := map[string]interface{}{
+		"http://schemas.example.com/emailaddress": "user@example.com",
+	}
+	result := ApplyAttributeMappings(attrs, []AttributeMapping{
+		{ExternalAttribute: "http://schemas.example.com/emailaddress", LocalAttribute: "email"},
+	})
+	s.Equal("user@example.com", result["email"])
+}
+
+func (s *IDPUtilsTestSuite) TestGetAttributeMappings_NilIDP() {
+	s.Nil(GetAttributeMappings(nil))
+}
+
+func (s *IDPUtilsTestSuite) TestGetAttributeMappings_NilAttributeConfiguration() {
+	s.Nil(GetAttributeMappings(&IDPDTO{}))
+}
+
+func (s *IDPUtilsTestSuite) TestGetAttributeMappings_ReturnsMappings() {
+	mappings := []AttributeMapping{{ExternalAttribute: "given_name", LocalAttribute: "firstName"}}
+	idpDTO := &IDPDTO{AttributeConfiguration: &AttributeConfiguration{
+		UserTypeResolution:        &UserTypeResolution{Default: "person"},
+		UserTypeAttributeMappings: []UserTypeAttributeMapping{{UserType: "person", Attributes: mappings}},
+	}}
+	s.Equal(mappings, GetAttributeMappings(idpDTO))
+}
+
+func (s *IDPUtilsTestSuite) TestGetMappedUserType_NilIDP() {
+	s.Equal("", GetMappedUserType(nil))
+}
+
+func (s *IDPUtilsTestSuite) TestGetMappedUserType_NilAttributeConfiguration() {
+	s.Equal("", GetMappedUserType(&IDPDTO{}))
+}
+
+func (s *IDPUtilsTestSuite) TestGetMappedUserType_ReturnsEntityType() {
+	s.Equal("person", GetMappedUserType(&IDPDTO{AttributeConfiguration: &AttributeConfiguration{
+		UserTypeResolution: &UserTypeResolution{Default: "person"},
+	}}))
+}