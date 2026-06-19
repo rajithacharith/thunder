thunder-id/thunderid

Add common input validation layer for API request payloads

backend/internal/ou/model.go
@@ -56,32 +56,32 @@ type OrganizationUnit struct {
 
 // OrganizationUnitRequest represents the request body for creating an organization unit.
 type OrganizationUnitRequest struct {
-	Handle          string  `json:"handle"`
-	Name            string  `json:"name"`
+	Handle          string  `json:"handle" native:"required,min=3,max=50"`
+	Name            string  `json:"name" native:"required,min=2,max=100"`
 	Description     string  `json:"description,omitempty"`
-	Parent          *string `json:"parent"`
+	Parent          *string `json:"parent" native:"omitempty,max=255"`
 	ThemeID         string  `json:"themeId,omitempty"`
 	LayoutID        string  `json:"layoutId,omitempty"`
-	LogoURL         string  `json:"logoUrl,omitempty"`
-	TosURI          string  `json:"tosUri,omitempty"`
-	PolicyURI       string  `json:"policyUri,omitempty"`
-	CookiePolicyURI string  `json:"cookiePolicyUri,omitempty"`
+	LogoURL         string  `json:"logoUrl,omitempty" native:"omitempty,url,max=2048"`
+	TosURI          string  `json:"tosUri,omitempty" native:"omitempty,url,max=2048"`
+	PolicyURI       string  `json:"policyUri,omitempty" native:"omitempty,url,max=2048"`
+	CookiePolicyURI string  `json:"cookiePolicyUri,omitempty" native:"omitempty,url,max=2048"`
 }
 
 // OrganizationUnitRequestWithID represents the request body for creating an organization unit
 // in import/declarative paths where preserving IDs is required.
 type OrganizationUnitRequestWithID struct {
-	ID              string  `json:"id" yaml:"id"`
-	Handle          string  `json:"handle" yaml:"handle"`
-	Name            string  `json:"name" yaml:"name"`
+	ID              string  `json:"id" yaml:"id" native:"required"`
+	Handle          string  `json:"handle" yaml:"handle" native:"required,min=3,max=50"`
+	Name            string  `json:"name" yaml:"name" native:"required,min=2,max=100"`
 	Description     string  `json:"description,omitempty" yaml:"description,omitempty"`
 	Parent          *string `json:"parent" yaml:"parent"`
 	ThemeID         string  `json:"themeId,omitempty" yaml:"theme_id,omitempty"`
 	LayoutID        string  `json:"layoutId,omitempty" yaml:"layout_id,omitempty"`
-	LogoURL         string  `json:"logoUrl,omitempty" yaml:"logo_url,omitempty"`
-	TosURI          string  `json:"tosUri,omitempty" yaml:"tos_uri,omitempty"`
-	PolicyURI       string  `json:"policyUri,omitempty" yaml:"policy_uri,omitempty"`
-	CookiePolicyURI string  `json:"cookiePolicyUri,omitempty" yaml:"cookie_policy_uri,omitempty"`
+	LogoURL         string  `json:"logoUrl,omitempty" yaml:"logo_url,omitempty" native:"omitempty,url,max=2048"`
+	TosURI          string  `json:"tosUri,omitempty" yaml:"tos_uri,omitempty" native:"omitempty,url,max=2048"`
+	PolicyURI       string  `json:"policyUri,omitempty" yaml:"policy_uri,omitempty" native:"omitempty,url,max=2048"`
+	CookiePolicyURI string  `json:"cookiePolicyUri,omitempty" yaml:"cookie_policy_uri,omitempty" native:"omitempty,url,max=2048"`
 }
 
 // OrganizationUnitListResponse represents the response for listing organization units with pagination.

backend/internal/resource/model.go
@@ -118,10 +118,10 @@ type ActionListResponse struct {
 type CreateResourceServerRequest struct {
 	Name        string             `json:"name"`
 	Description string             `json:"description,omitempty"`
-	Handle      string             `json:"handle,omitempty"`
+	Handle      string             `json:"handle,omitempty" native:"required,max=100"`
 	Identifier  string             `json:"identifier,omitempty"`
 	Type        ResourceServerType `json:"type,omitempty"`
-	OUID        string             `json:"ouId"`
+	OUID        string             `json:"ouId" native:"required"`
 	Delimiter   string             `json:"delimiter,omitempty"`
 }
 
@@ -129,35 +129,35 @@ type CreateResourceServerRequest struct {
 type UpdateResourceServerRequest struct {
 	Name        string `json:"name"`
 	Description string `json:"description,omitempty"`
-	Handle      string `json:"handle,omitempty"`
+	Handle      string `json:"handle,omitempty" native:"required,max=100"`
 	Identifier  string `json:"identifier,omitempty"`
-	OUID        string `json:"ouId"`
+	OUID        string `json:"ouId" native:"required"`
 }
 
 // CreateResourceRequest represents the request to create a resource.
 type CreateResourceRequest struct {
 	Name        string  `json:"name"`
-	Handle      string  `json:"handle"`
+	Handle      string  `json:"handle" native:"required,max=100"`
 	Description string  `json:"description,omitempty"`
 	Parent      *string `json:"parent"`
 }
 
 // UpdateResourceRequest represents the request to update a resource.
 type UpdateResourceRequest struct {
-	Name        string `json:"name"`
+	Name        string `json:"name" native:"required"`
 	Description string `json:"description,omitempty"`
 }
 
 // CreateActionRequest represents the request to create an action.
 type CreateActionRequest struct {
-	Name        string `json:"name"`
-	Handle      string `json:"handle"`
+	Name        string `json:"name" native:"required"`
+	Handle      string `json:"handle" native:"required,max=50"`
 	Description string `json:"description,omitempty"`
 }
 
 // UpdateActionRequest represents the request to update an action.
 type UpdateActionRequest struct {
-	Name        string `json:"name"`
+	Name        string `json:"name" native:"required"`
 	Description string `json:"description,omitempty"`
 }
 

rajithacharith/thunder

backend/internal/ou/model.go
@@ -56,32 +56,32 @@ type OrganizationUnit struct {
 
 // OrganizationUnitRequest represents the request body for creating an organization unit.
 type OrganizationUnitRequest struct {
-	Handle          string  `json:"handle"`
-	Name            string  `json:"name"`
+	Handle          string  `json:"handle" native:"required,min=3,max=50"`
+	Name            string  `json:"name" native:"required,min=2,max=100"`
 	Description     string  `json:"description,omitempty"`
-	Parent          *string `json:"parent"`
+	Parent          *string `json:"parent" native:"omitempty,max=255"`
 	ThemeID         string  `json:"themeId,omitempty"`
 	LayoutID        string  `json:"layoutId,omitempty"`
-	LogoURL         string  `json:"logoUrl,omitempty"`
-	TosURI          string  `json:"tosUri,omitempty"`
-	PolicyURI       string  `json:"policyUri,omitempty"`
-	CookiePolicyURI string  `json:"cookiePolicyUri,omitempty"`
+	LogoURL         string  `json:"logoUrl,omitempty" native:"omitempty,url,max=2048"`
+	TosURI          string  `json:"tosUri,omitempty" native:"omitempty,url,max=2048"`
+	PolicyURI       string  `json:"policyUri,omitempty" native:"omitempty,url,max=2048"`
+	CookiePolicyURI string  `json:"cookiePolicyUri,omitempty" native:"omitempty,url,max=2048"`
 }
 
 // OrganizationUnitRequestWithID represents the request body for creating an organization unit
 // in import/declarative paths where preserving IDs is required.
 type OrganizationUnitRequestWithID struct {
-	ID              string  `json:"id" yaml:"id"`
-	Handle          string  `json:"handle" yaml:"handle"`
-	Name            string  `json:"name" yaml:"name"`
+	ID              string  `json:"id" yaml:"id" native:"required"`
+	Handle          string  `json:"handle" yaml:"handle" native:"required,min=3,max=50"`
+	Name            string  `json:"name" yaml:"name" native:"required,min=2,max=100"`
 	Description     string  `json:"description,omitempty" yaml:"description,omitempty"`
 	Parent          *string `json:"parent" yaml:"parent"`
 	ThemeID         string  `json:"themeId,omitempty" yaml:"themeId,omitempty"`
 	LayoutID        string  `json:"layoutId,omitempty" yaml:"layoutId,omitempty"`
-	LogoURL         string  `json:"logoUrl,omitempty" yaml:"logoUrl,omitempty"`
-	TosURI          string  `json:"tosUri,omitempty" yaml:"tosUri,omitempty"`
-	PolicyURI       string  `json:"policyUri,omitempty" yaml:"policyUri,omitempty"`
-	CookiePolicyURI string  `json:"cookiePolicyUri,omitempty" yaml:"cookiePolicyUri,omitempty"`
+	LogoURL         string  `json:"logoUrl,omitempty" yaml:"logoUrl,omitempty" native:"omitempty,url,max=2048"`
+	TosURI          string  `json:"tosUri,omitempty" yaml:"tosUri,omitempty" native:"omitempty,url,max=2048"`
+	PolicyURI       string  `json:"policyUri,omitempty" yaml:"policyUri,omitempty" native:"omitempty,url,max=2048"`
+	CookiePolicyURI string  `json:"cookiePolicyUri,omitempty" yaml:"cookiePolicyUri,omitempty" native:"url,max=2048"`
 }
 
 // OrganizationUnitListResponse represents the response for listing organization units with pagination.

backend/internal/resource/model.go
@@ -116,12 +116,12 @@ type ActionListResponse struct {
 
 // CreateResourceServerRequest represents the request to create a resource server.
 type CreateResourceServerRequest struct {
-	Name        string             `json:"name"`
+	Name        string             `json:"name" native:"required"`
 	Description string             `json:"description,omitempty"`
-	Handle      string             `json:"handle,omitempty"`
+	Handle      string             `json:"handle,omitempty" native:"omitempty,max=100"`
 	Identifier  string             `json:"identifier,omitempty"`
 	Type        ResourceServerType `json:"type,omitempty"`
-	OUID        string             `json:"ouId"`
+	OUID        string             `json:"ouId" native:"required"`
 	Delimiter   string             `json:"delimiter,omitempty"`
 }
 
@@ -129,35 +129,35 @@ type CreateResourceServerRequest struct {
 type UpdateResourceServerRequest struct {
 	Name        string `json:"name"`
 	Description string `json:"description,omitempty"`
-	Handle      string `json:"handle,omitempty"`
+	Handle      string `json:"handle,omitempty" native:"omitempty,max=100"`
 	Identifier  string `json:"identifier,omitempty"`
-	OUID        string `json:"ouId"`
+	OUID        string `json:"ouId" native:"required"`
 }
 
 // CreateResourceRequest represents the request to create a resource.
 type CreateResourceRequest struct {
 	Name        string  `json:"name"`
-	Handle      string  `json:"handle"`
+	Handle      string  `json:"handle" native:"required,max=100"`
 	Description string  `json:"description,omitempty"`
 	Parent      *string `json:"parent"`
 }
 
 // UpdateResourceRequest represents the request to update a resource.
 type UpdateResourceRequest struct {
-	Name        string `json:"name"`
+	Name        string `json:"name" native:"required"`
 	Description string `json:"description,omitempty"`
 }
 
 // CreateActionRequest represents the request to create an action.
 type CreateActionRequest struct {
-	Name        string `json:"name"`
-	Handle      string `json:"handle"`
+	Name        string `json:"name" native:"required"`
+	Handle      string `json:"handle" native:"required,max=50"`
 	Description string `json:"description,omitempty"`
 }
 
 // UpdateActionRequest represents the request to update an action.
 type UpdateActionRequest struct {
-	Name        string `json:"name"`
+	Name        string `json:"name" native:"required"`
 	Description string `json:"description,omitempty"`
 }
 