thunder-id/thunderid

Add support to check theme usage and show it in delete

backend/internal/application/service_test.go
@@ -3407,3 +3407,107 @@ func (suite *ServiceTestSuite) TestValidateApplicationFields_FlowHandleResolutio
 	assert.NotNil(suite.T(), svcErr)
 	assert.Equal(suite.T(), ErrorInvalidRequestFormat.Code, svcErr.Code)
 }
+
+// --- GetApplicationsByThemeID tests ---
+
+func (suite *ServiceTestSuite) TestGetApplicationsByThemeID_InboundClientError() {
+	service, mockStore := suite.setupTestService()
+	mockStore.EXPECT().
+		GetEntityIDsByThemeID(mock.Anything, "theme-1", 10, 0).
+		Return(nil, 0, errors.New("store error"))
+
+	result, total, svcErr := service.GetApplicationsByThemeID(context.Background(), "theme-1", 10, 0)
+	assert.Nil(suite.T(), result)
+	assert.Equal(suite.T(), 0, total)
+	assert.NotNil(suite.T(), svcErr)
+}
+
+func (suite *ServiceTestSuite) TestGetApplicationsByThemeID_EmptyIDs() {
+	service, mockStore := suite.setupTestService()
+	mockStore.EXPECT().
+		GetEntityIDsByThemeID(mock.Anything, "theme-1", 10, 0).
+		Return([]string{}, 0, nil)
+
+	result, total, svcErr := service.GetApplicationsByThemeID(context.Background(), "theme-1", 10, 0)
+	assert.Nil(suite.T(), svcErr)
+	assert.Equal(suite.T(), 0, total)
+	assert.Empty(suite.T(), result)
+}
+
+func (suite *ServiceTestSuite) TestGetApplicationsByThemeID_EntityProviderError() {
+	service, mockStore := suite.setupTestService()
+	mockStore.EXPECT().
+		GetEntityIDsByThemeID(mock.Anything, "theme-1", 10, 0).
+		Return([]string{"app-1"}, 1, nil)
+
+	ep := resetEntityProviderMethod(service, "GetEntitiesByIDs")
+	epErr := entityprovider.NewEntityProviderError(entityprovider.ErrorCodeEntityNotFound, "error", "")
+	ep.On("GetEntitiesByIDs", []string{"app-1"}).Return([]entityprovider.Entity{}, epErr)
+
+	result, total, svcErr := service.GetApplicationsByThemeID(context.Background(), "theme-1", 10, 0)
+	assert.Nil(suite.T(), result)
+	assert.Equal(suite.T(), 0, total)
+	assert.NotNil(suite.T(), svcErr)
+}
+
+func (suite *ServiceTestSuite) TestGetApplicationsByThemeID_Success() {
+	service, mockStore := suite.setupTestService()
+	mockStore.EXPECT().
+		GetEntityIDsByThemeID(mock.Anything, "theme-1", 10, 0).
+		Return([]string{"app-1", "app-2"}, 2, nil)
+
+	sysAttrs1, _ := json.Marshal(map[string]interface{}{"name": "Portal App"})
+	sysAttrs2, _ := json.Marshal(map[string]interface{}{"name": "Admin App"})
+	ep := resetEntityProviderMethod(service, "GetEntitiesByIDs")
+	ep.On("GetEntitiesByIDs", []string{"app-1", "app-2"}).Return([]entityprovider.Entity{
+		{ID: "app-1", SystemAttributes: sysAttrs1},
+		{ID: "app-2", SystemAttributes: sysAttrs2},
+	}, (*entityprovider.EntityProviderError)(nil))
+
+	result, total, svcErr := service.GetApplicationsByThemeID(context.Background(), "theme-1", 10, 0)
+	assert.Nil(suite.T(), svcErr)
+	assert.Equal(suite.T(), 2, total)
+	require.Len(suite.T(), result, 2)
+	assert.Equal(suite.T(), "app-1", result[0].ID)
+	assert.Equal(suite.T(), "Portal App", result[0].DisplayName)
+	assert.Equal(suite.T(), "app-2", result[1].ID)
+	assert.Equal(suite.T(), "Admin App", result[1].DisplayName)
+}
+
+func (suite *ServiceTestSuite) TestGetApplicationsByThemeID_NoSystemAttributes() {
+	service, mockStore := suite.setupTestService()
+	mockStore.EXPECT().
+		GetEntityIDsByThemeID(mock.Anything, "theme-1", 10, 0).
+		Return([]string{"app-1"}, 1, nil)
+
+	ep := resetEntityProviderMethod(service, "GetEntitiesByIDs")
+	ep.On("GetEntitiesByIDs", []string{"app-1"}).Return([]entityprovider.Entity{
+		{ID: "app-1"},
+	}, (*entityprovider.EntityProviderError)(nil))
+
+	result, total, svcErr := service.GetApplicationsByThemeID(context.Background(), "theme-1", 10, 0)
+	assert.Nil(suite.T(), svcErr)
+	assert.Equal(suite.T(), 1, total)
+	require.Len(suite.T(), result, 1)
+	assert.Equal(suite.T(), "app-1", result[0].ID)
+	assert.Equal(suite.T(), "", result[0].DisplayName)
+}
+
+func (suite *ServiceTestSuite) TestGetApplicationsByThemeID_SystemAttributesWithoutName() {
+	service, mockStore := suite.setupTestService()
+	mockStore.EXPECT().
+		GetEntityIDsByThemeID(mock.Anything, "theme-1", 10, 0).
+		Return([]string{"app-1"}, 1, nil)
+
+	sysAttrs, _ := json.Marshal(map[string]interface{}{"description": "some desc"})
+	ep := resetEntityProviderMethod(service, "GetEntitiesByIDs")
+	ep.On("GetEntitiesByIDs", []string{"app-1"}).Return([]entityprovider.Entity{
+		{ID: "app-1", SystemAttributes: sysAttrs},
+	}, (*entityprovider.EntityProviderError)(nil))
+
+	result, total, svcErr := service.GetApplicationsByThemeID(context.Background(), "theme-1", 10, 0)
+	assert.Nil(suite.T(), svcErr)
+	assert.Equal(suite.T(), 1, total)
+	require.Len(suite.T(), result, 1)
+	assert.Equal(suite.T(), "", result[0].DisplayName)
+}

backend/tests/mocks/design/thememock/ThemeMgtServiceInterface_mock.go
@@ -314,6 +314,88 @@ func (_c *ThemeMgtServiceInterfaceMock_GetThemeList_Call) RunAndReturn(run func(
 	return _c
 }
 
+// GetThemeUsages provides a mock function for the type ThemeMgtServiceInterfaceMock
+func (_mock *ThemeMgtServiceInterfaceMock) GetThemeUsages(ctx context.Context, id string, limit int, offset int) (*thememgt.ThemeUsagesResponse, *serviceerror.ServiceError) {
+	ret := _mock.Called(ctx, id, limit, offset)
+
+	if len(ret) == 0 {
+		panic("no return value specified for GetThemeUsages")
+	}
+
+	var r0 *thememgt.ThemeUsagesResponse
+	var r1 *serviceerror.ServiceError
+	if returnFunc, ok := ret.Get(0).(func(context.Context, string, int, int) (*thememgt.ThemeUsagesResponse, *serviceerror.ServiceError)); ok {
+		return returnFunc(ctx, id, limit, offset)
+	}
+	if returnFunc, ok := ret.Get(0).(func(context.Context, string, int, int) *thememgt.ThemeUsagesResponse); ok {
+		r0 = returnFunc(ctx, id, limit, offset)
+	} else {
+		if ret.Get(0) != nil {
+			r0 = ret.Get(0).(*thememgt.ThemeUsagesResponse)
+		}
+	}
+	if returnFunc, ok := ret.Get(1).(func(context.Context, string, int, int) *serviceerror.ServiceError); ok {
+		r1 = returnFunc(ctx, id, limit, offset)
+	} else {
+		if ret.Get(1) != nil {
+			r1 = ret.Get(1).(*serviceerror.ServiceError)
+		}
+	}
+	return r0, r1
+}
+
+// ThemeMgtServiceInterfaceMock_GetThemeUsages_Call is a *mock.Call that shadows Run/Return methods with type explicit version for method 'GetThemeUsages'
+type ThemeMgtServiceInterfaceMock_GetThemeUsages_Call struct {
+	*mock.Call
+}
+
+// GetThemeUsages is a helper method to define mock.On call
+//   - ctx context.Context
+//   - id string
+//   - limit int
+//   - offset int
+func (_e *ThemeMgtServiceInterfaceMock_Expecter) GetThemeUsages(ctx interface{}, id interface{}, limit interface{}, offset interface{}) *ThemeMgtServiceInterfaceMock_GetThemeUsages_Call {
+	return &ThemeMgtServiceInterfaceMock_GetThemeUsages_Call{Call: _e.mock.On("GetThemeUsages", ctx, id, limit, offset)}
+}
+
+func (_c *ThemeMgtServiceInterfaceMock_GetThemeUsages_Call) Run(run func(ctx context.Context, id string, limit int, offset int)) *ThemeMgtServiceInterfaceMock_GetThemeUsages_Call {
+	_c.Call.Run(func(args mock.Arguments) {
+		var arg0 context.Context
+		if args[0] != nil {
+			arg0 = args[0].(context.Context)
+		}
+		var arg1 string
+		if args[1] != nil {
+			arg1 = args[1].(string)
+		}
+		var arg2 int
+		if args[2] != nil {
+			arg2 = args[2].(int)
+		}
+		var arg3 int
+		if args[3] != nil {
+			arg3 = args[3].(int)
+		}
+		run(
+			arg0,
+			arg1,
+			arg2,
+			arg3,
+		)
+	})
+	return _c
+}
+
+func (_c *ThemeMgtServiceInterfaceMock_GetThemeUsages_Call) Return(themeUsagesResponse *thememgt.ThemeUsagesResponse, serviceError *serviceerror.ServiceError) *ThemeMgtServiceInterfaceMock_GetThemeUsages_Call {
+	_c.Call.Return(themeUsagesResponse, serviceError)
+	return _c
+}
+
+func (_c *ThemeMgtServiceInterfaceMock_GetThemeUsages_Call) RunAndReturn(run func(ctx context.Context, id string, limit int, offset int) (*thememgt.ThemeUsagesResponse, *serviceerror.ServiceError)) *ThemeMgtServiceInterfaceMock_GetThemeUsages_Call {
+	_c.Call.Return(run)
+	return _c
+}
+
 // IsThemeExist provides a mock function for the type ThemeMgtServiceInterfaceMock
 func (_mock *ThemeMgtServiceInterfaceMock) IsThemeExist(ctx context.Context, id string) (bool, *serviceerror.ServiceError) {
 	ret := _mock.Called(ctx, id)
@@ -379,6 +461,46 @@ func (_c *ThemeMgtServiceInterfaceMock_IsThemeExist_Call) Return(b bool, service
 
 func (_c *ThemeMgtServiceInterfaceMock_IsThemeExist_Call) RunAndReturn(run func(ctx context.Context, id string) (bool, *serviceerror.ServiceError)) *ThemeMgtServiceInterfaceMock_IsThemeExist_Call {
 	_c.Call.Return(run)
+	return _c
+}
+
+// SetApplicationReader provides a mock function for the type ThemeMgtServiceInterfaceMock
+func (_mock *ThemeMgtServiceInterfaceMock) SetApplicationReader(r thememgt.ApplicationUsageReader) {
+	_mock.Called(r)
+	return
+}
+
+// ThemeMgtServiceInterfaceMock_SetApplicationReader_Call is a *mock.Call that shadows Run/Return methods with type explicit version for method 'SetApplicationReader'
+type ThemeMgtServiceInterfaceMock_SetApplicationReader_Call struct {
+	*mock.Call
+}
+
+// SetApplicationReader is a helper method to define mock.On call
+//   - r thememgt.ApplicationUsageReader
+func (_e *ThemeMgtServiceInterfaceMock_Expecter) SetApplicationReader(r interface{}) *ThemeMgtServiceInterfaceMock_SetApplicationReader_Call {
+	return &ThemeMgtServiceInterfaceMock_SetApplicationReader_Call{Call: _e.mock.On("SetApplicationReader", r)}
+}
+
+func (_c *ThemeMgtServiceInterfaceMock_SetApplicationReader_Call) Run(run func(r thememgt.ApplicationUsageReader)) *ThemeMgtServiceInterfaceMock_SetApplicationReader_Call {
+	_c.Call.Run(func(args mock.Arguments) {
+		var arg0 thememgt.ApplicationUsageReader
+		if args[0] != nil {
+			arg0 = args[0].(thememgt.ApplicationUsageReader)
+		}
+		run(
+			arg0,
+		)
+	})
+	return _c
+}
+
+func (_c *ThemeMgtServiceInterfaceMock_SetApplicationReader_Call) Return() *ThemeMgtServiceInterfaceMock_SetApplicationReader_Call {
+	_c.Call.Return()
+	return _c
+}
+
+func (_c *ThemeMgtServiceInterfaceMock_SetApplicationReader_Call) RunAndReturn(run func(r thememgt.ApplicationUsageReader)) *ThemeMgtServiceInterfaceMock_SetApplicationReader_Call {
+	_c.Run(run)
 	return _c
 }
 

rajithacharith/thunder

backend/internal/application/service_test.go
@@ -39,7 +39,9 @@ import (
 	"github.com/thunder-id/thunderid/internal/inboundclient"
 	inboundmodel "github.com/thunder-id/thunderid/internal/inboundclient/model"
 	"github.com/thunder-id/thunderid/internal/system/config"
+	serverconst "github.com/thunder-id/thunderid/internal/system/constants"
 	"github.com/thunder-id/thunderid/internal/system/log"
+	"github.com/thunder-id/thunderid/internal/system/resourcedependency"
 	"github.com/thunder-id/thunderid/pkg/thunderidengine/providers"
 	"github.com/thunder-id/thunderid/tests/mocks/entityprovidermock"
 	"github.com/thunder-id/thunderid/tests/mocks/i18n/mgtmock"
@@ -3318,6 +3320,141 @@ func (suite *ServiceTestSuite) TestValidateApplicationFields_FlowHandleResolutio
 
 	assert.NotNil(suite.T(), svcErr)
 	assert.Equal(suite.T(), ErrorInvalidRequestFormat.Code, svcErr.Code)
+}
+
+// --- GetResourceDependencies tests ---
+
+func (suite *ServiceTestSuite) TestGetResourceDependencies_UnknownResourceType() {
+	service, _ := suite.setupTestService()
+
+	result, err := service.GetResourceDependencies(context.Background(), "unknown", "id-1")
+	assert.NoError(suite.T(), err)
+	assert.Empty(suite.T(), result)
+}
+
+func (suite *ServiceTestSuite) TestGetResourceDependencies_InboundClientError() {
+	service, mockStore := suite.setupTestService()
+	mockStore.EXPECT().
+		GetEntityIDsByThemeID(mock.Anything, "theme-1", serverconst.MaxCompositeStoreRecords, 0).
+		Return(nil, 0, errors.New("store error"))
+
+	result, err := service.GetResourceDependencies(
+		context.Background(), resourcedependency.ResourceTypeTheme, "theme-1")
+	assert.Nil(suite.T(), result)
+	assert.Error(suite.T(), err)
+}
+
+func (suite *ServiceTestSuite) TestGetResourceDependencies_EmptyIDs() {
+	service, mockStore := suite.setupTestService()
+	mockStore.EXPECT().
+		GetEntityIDsByThemeID(mock.Anything, "theme-1", serverconst.MaxCompositeStoreRecords, 0).
+		Return([]string{}, 0, nil)
+
+	result, err := service.GetResourceDependencies(
+		context.Background(), resourcedependency.ResourceTypeTheme, "theme-1")
+	assert.NoError(suite.T(), err)
+	assert.Empty(suite.T(), result)
+}
+
+func (suite *ServiceTestSuite) TestGetResourceDependencies_EntityProviderError() {
+	service, mockStore := suite.setupTestService()
+	mockStore.EXPECT().
+		GetEntityIDsByThemeID(mock.Anything, "theme-1", serverconst.MaxCompositeStoreRecords, 0).
+		Return([]string{"app-1"}, 1, nil)
+
+	ep := resetEntityProviderMethod(service, "GetEntitiesByIDs")
+	epErr := entityprovider.NewEntityProviderError(entityprovider.ErrorCodeEntityNotFound, "error", "")
+	ep.On("GetEntitiesByIDs", []string{"app-1"}).Return([]providers.Entity{}, epErr)
+
+	result, err := service.GetResourceDependencies(
+		context.Background(), resourcedependency.ResourceTypeTheme, "theme-1")
+	assert.Nil(suite.T(), result)
+	assert.Error(suite.T(), err)
+}
+
+func (suite *ServiceTestSuite) TestGetResourceDependencies_Success() {
+	service, mockStore := suite.setupTestService()
+	mockStore.EXPECT().
+		GetEntityIDsByThemeID(mock.Anything, "theme-1", serverconst.MaxCompositeStoreRecords, 0).
+		Return([]string{"app-1", "app-2"}, 2, nil)
+
+	sysAttrs1, _ := json.Marshal(map[string]interface{}{"name": "Portal App"})
+	sysAttrs2, _ := json.Marshal(map[string]interface{}{"name": "Admin App"})
+	ep := resetEntityProviderMethod(service, "GetEntitiesByIDs")
+	ep.On("GetEntitiesByIDs", []string{"app-1", "app-2"}).Return([]providers.Entity{
+		{ID: "app-1", Category: providers.EntityCategoryApp, SystemAttributes: sysAttrs1},
+		{ID: "app-2", Category: providers.EntityCategoryApp, SystemAttributes: sysAttrs2},
+	}, (*entityprovider.EntityProviderError)(nil))
+
+	result, err := service.GetResourceDependencies(
+		context.Background(), resourcedependency.ResourceTypeTheme, "theme-1")
+	assert.NoError(suite.T(), err)
+	require.Len(suite.T(), result, 2)
+	assert.Equal(suite.T(), resourcedependency.ResourceTypeApplication, result[0].ResourceType)
+	assert.Equal(suite.T(), resourcedependency.BehaviorFallback, result[0].BehaviorOnDelete)
+	assert.Equal(suite.T(), "app-1", result[0].ID)
+	assert.Equal(suite.T(), "Portal App", result[0].DisplayName)
+	assert.Equal(suite.T(), "app-2", result[1].ID)
+	assert.Equal(suite.T(), "Admin App", result[1].DisplayName)
+}
+
+// Agents share the inbound-client store; the application provider must skip non-app entities.
+func (suite *ServiceTestSuite) TestGetResourceDependencies_FiltersOutNonAppEntities() {
+	service, mockStore := suite.setupTestService()
+	mockStore.EXPECT().
+		GetEntityIDsByThemeID(mock.Anything, "theme-1", serverconst.MaxCompositeStoreRecords, 0).
+		Return([]string{"app-1", "agent-1"}, 2, nil)
+
+	sysAttrs, _ := json.Marshal(map[string]interface{}{"name": "Portal App"})
+	ep := resetEntityProviderMethod(service, "GetEntitiesByIDs")
+	ep.On("GetEntitiesByIDs", []string{"app-1", "agent-1"}).Return([]providers.Entity{
+		{ID: "app-1", Category: providers.EntityCategoryApp, SystemAttributes: sysAttrs},
+		{ID: "agent-1", Category: providers.EntityCategoryAgent},
+	}, (*entityprovider.EntityProviderError)(nil))
+
+	result, err := service.GetResourceDependencies(
+		context.Background(), resourcedependency.ResourceTypeTheme, "theme-1")
+	assert.NoError(suite.T(), err)
+	require.Len(suite.T(), result, 1)
+	assert.Equal(suite.T(), "app-1", result[0].ID)
+}
+
+func (suite *ServiceTestSuite) TestGetResourceDependencies_NoSystemAttributes() {
+	service, mockStore := suite.setupTestService()
+	mockStore.EXPECT().
+		GetEntityIDsByThemeID(mock.Anything, "theme-1", serverconst.MaxCompositeStoreRecords, 0).
+		Return([]string{"app-1"}, 1, nil)
+
+	ep := resetEntityProviderMethod(service, "GetEntitiesByIDs")
+	ep.On("GetEntitiesByIDs", []string{"app-1"}).Return([]providers.Entity{
+		{ID: "app-1", Category: providers.EntityCategoryApp},
+	}, (*entityprovider.EntityProviderError)(nil))
+
+	result, err := service.GetResourceDependencies(
+		context.Background(), resourcedependency.ResourceTypeTheme, "theme-1")
+	assert.NoError(suite.T(), err)
+	require.Len(suite.T(), result, 1)
+	assert.Equal(suite.T(), "app-1", result[0].ID)
+	assert.Equal(suite.T(), "", result[0].DisplayName)
+}
+
+func (suite *ServiceTestSuite) TestGetResourceDependencies_SystemAttributesWithoutName() {
+	service, mockStore := suite.setupTestService()
+	mockStore.EXPECT().
+		GetEntityIDsByThemeID(mock.Anything, "theme-1", serverconst.MaxCompositeStoreRecords, 0).
+		Return([]string{"app-1"}, 1, nil)
+
+	sysAttrs, _ := json.Marshal(map[string]interface{}{"description": "some desc"})
+	ep := resetEntityProviderMethod(service, "GetEntitiesByIDs")
+	ep.On("GetEntitiesByIDs", []string{"app-1"}).Return([]providers.Entity{
+		{ID: "app-1", Category: providers.EntityCategoryApp, SystemAttributes: sysAttrs},
+	}, (*entityprovider.EntityProviderError)(nil))
+
+	result, err := service.GetResourceDependencies(
+		context.Background(), resourcedependency.ResourceTypeTheme, "theme-1")
+	assert.NoError(suite.T(), err)
+	require.Len(suite.T(), result, 1)
+	assert.Equal(suite.T(), "", result[0].DisplayName)
 }
 
 func (suite *ServiceTestSuite) TestCreateApplication_CreateInboundClientFailsAndCompensationFails() {

backend/tests/mocks/design/thememock/ThemeMgtServiceInterface_mock.go
@@ -9,6 +9,7 @@ import (
 
 	mock "github.com/stretchr/testify/mock"
 	"github.com/thunder-id/thunderid/internal/design/theme/mgt"
+	"github.com/thunder-id/thunderid/internal/system/resourcedependency"
 	"github.com/thunder-id/thunderid/pkg/thunderidengine/common"
 )
 
@@ -314,6 +315,88 @@ func (_c *ThemeMgtServiceInterfaceMock_GetThemeList_Call) RunAndReturn(run func(
 	return _c
 }
 
+// GetThemeUsages provides a mock function for the type ThemeMgtServiceInterfaceMock
+func (_mock *ThemeMgtServiceInterfaceMock) GetThemeUsages(ctx context.Context, id string, limit int, offset int) (*resourcedependency.DependenciesResponse, *common.ServiceError) {
+	ret := _mock.Called(ctx, id, limit, offset)
+
+	if len(ret) == 0 {
+		panic("no return value specified for GetThemeUsages")
+	}
+
+	var r0 *resourcedependency.DependenciesResponse
+	var r1 *common.ServiceError
+	if returnFunc, ok := ret.Get(0).(func(context.Context, string, int, int) (*resourcedependency.DependenciesResponse, *common.ServiceError)); ok {
+		return returnFunc(ctx, id, limit, offset)
+	}
+	if returnFunc, ok := ret.Get(0).(func(context.Context, string, int, int) *resourcedependency.DependenciesResponse); ok {
+		r0 = returnFunc(ctx, id, limit, offset)
+	} else {
+		if ret.Get(0) != nil {
+			r0 = ret.Get(0).(*resourcedependency.DependenciesResponse)
+		}
+	}
+	if returnFunc, ok := ret.Get(1).(func(context.Context, string, int, int) *common.ServiceError); ok {
+		r1 = returnFunc(ctx, id, limit, offset)
+	} else {
+		if ret.Get(1) != nil {
+			r1 = ret.Get(1).(*common.ServiceError)
+		}
+	}
+	return r0, r1
+}
+
+// ThemeMgtServiceInterfaceMock_GetThemeUsages_Call is a *mock.Call that shadows Run/Return methods with type explicit version for method 'GetThemeUsages'
+type ThemeMgtServiceInterfaceMock_GetThemeUsages_Call struct {
+	*mock.Call
+}
+
+// GetThemeUsages is a helper method to define mock.On call
+//   - ctx context.Context
+//   - id string
+//   - limit int
+//   - offset int
+func (_e *ThemeMgtServiceInterfaceMock_Expecter) GetThemeUsages(ctx interface{}, id interface{}, limit interface{}, offset interface{}) *ThemeMgtServiceInterfaceMock_GetThemeUsages_Call {
+	return &ThemeMgtServiceInterfaceMock_GetThemeUsages_Call{Call: _e.mock.On("GetThemeUsages", ctx, id, limit, offset)}
+}
+
+func (_c *ThemeMgtServiceInterfaceMock_GetThemeUsages_Call) Run(run func(ctx context.Context, id string, limit int, offset int)) *ThemeMgtServiceInterfaceMock_GetThemeUsages_Call {
+	_c.Call.Run(func(args mock.Arguments) {
+		var arg0 context.Context
+		if args[0] != nil {
+			arg0 = args[0].(context.Context)
+		}
+		var arg1 string
+		if args[1] != nil {
+			arg1 = args[1].(string)
+		}
+		var arg2 int
+		if args[2] != nil {
+			arg2 = args[2].(int)
+		}
+		var arg3 int
+		if args[3] != nil {
+			arg3 = args[3].(int)
+		}
+		run(
+			arg0,
+			arg1,
+			arg2,
+			arg3,
+		)
+	})
+	return _c
+}
+
+func (_c *ThemeMgtServiceInterfaceMock_GetThemeUsages_Call) Return(dependenciesResponse *resourcedependency.DependenciesResponse, serviceError *common.ServiceError) *ThemeMgtServiceInterfaceMock_GetThemeUsages_Call {
+	_c.Call.Return(dependenciesResponse, serviceError)
+	return _c
+}
+
+func (_c *ThemeMgtServiceInterfaceMock_GetThemeUsages_Call) RunAndReturn(run func(ctx context.Context, id string, limit int, offset int) (*resourcedependency.DependenciesResponse, *common.ServiceError)) *ThemeMgtServiceInterfaceMock_GetThemeUsages_Call {
+	_c.Call.Return(run)
+	return _c
+}
+
 // IsThemeExist provides a mock function for the type ThemeMgtServiceInterfaceMock
 func (_mock *ThemeMgtServiceInterfaceMock) IsThemeExist(ctx context.Context, id string) (bool, *common.ServiceError) {
 	ret := _mock.Called(ctx, id)
@@ -379,6 +462,46 @@ func (_c *ThemeMgtServiceInterfaceMock_IsThemeExist_Call) Return(b bool, service
 
 func (_c *ThemeMgtServiceInterfaceMock_IsThemeExist_Call) RunAndReturn(run func(ctx context.Context, id string) (bool, *common.ServiceError)) *ThemeMgtServiceInterfaceMock_IsThemeExist_Call {
 	_c.Call.Return(run)
+	return _c
+}
+
+// SetDependencyRegistry provides a mock function for the type ThemeMgtServiceInterfaceMock
+func (_mock *ThemeMgtServiceInterfaceMock) SetDependencyRegistry(r resourcedependency.Registry) {
+	_mock.Called(r)
+	return
+}
+
+// ThemeMgtServiceInterfaceMock_SetDependencyRegistry_Call is a *mock.Call that shadows Run/Return methods with type explicit version for method 'SetDependencyRegistry'
+type ThemeMgtServiceInterfaceMock_SetDependencyRegistry_Call struct {
+	*mock.Call
+}
+
+// SetDependencyRegistry is a helper method to define mock.On call
+//   - r resourcedependency.Registry
+func (_e *ThemeMgtServiceInterfaceMock_Expecter) SetDependencyRegistry(r interface{}) *ThemeMgtServiceInterfaceMock_SetDependencyRegistry_Call {
+	return &ThemeMgtServiceInterfaceMock_SetDependencyRegistry_Call{Call: _e.mock.On("SetDependencyRegistry", r)}
+}
+
+func (_c *ThemeMgtServiceInterfaceMock_SetDependencyRegistry_Call) Run(run func(r resourcedependency.Registry)) *ThemeMgtServiceInterfaceMock_SetDependencyRegistry_Call {
+	_c.Call.Run(func(args mock.Arguments) {
+		var arg0 resourcedependency.Registry
+		if args[0] != nil {
+			arg0 = args[0].(resourcedependency.Registry)
+		}
+		run(
+			arg0,
+		)
+	})
+	return _c
+}
+
+func (_c *ThemeMgtServiceInterfaceMock_SetDependencyRegistry_Call) Return() *ThemeMgtServiceInterfaceMock_SetDependencyRegistry_Call {
+	_c.Call.Return()
+	return _c
+}
+
+func (_c *ThemeMgtServiceInterfaceMock_SetDependencyRegistry_Call) RunAndReturn(run func(r resourcedependency.Registry)) *ThemeMgtServiceInterfaceMock_SetDependencyRegistry_Call {
+	_c.Run(run)
 	return _c
 }
 